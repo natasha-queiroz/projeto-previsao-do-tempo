f1d9b95f8c70645eadec3b7b7bf9a3d1a289af04
Natasha Queiroz <natashaqueiroz1612@gmail.com>
2025-01-11 13:31:44 -0300

    html, script

--- script.js
+++ script.js
@@ -21,13 +21,13 @@ document.querySelector('#search').addEventListener('submit', async (event) => {
             tempMax: json.main.temp_max,
             tempMin: json.main.temp_min,
             description: json.weather[0].description,
-            temoIcon: json.weather[0].icon,
+            tempIcon: json.weather[0].icon,
             windSpeed: json.wind.speed,
             humidity: json.main.humidity,
 
         });
     }else{
-        showAlert('Nao foi possivel localizer...')
+        showAlert('Nao foi possivel localizar...')
     }
 
 });
@@ -43,10 +43,14 @@ function showInfo(json){
 
     document.querySelector('#temp_description').innerHTML = `${json.description}`;
     
-    document.querySelector('#temp_img').setAttribute('src', ` https://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
-    
+    document.querySelector('#temp_img').setAttribute('src', `https://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
 
+    document.querySelector('#temp_max').innerHTML = `${json.tempMax.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
 
+    document.querySelector('#temp_min').innerHTML = `${json.tempMin.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
+
+    document.querySelector('#humidity').innerHTML = `${json.humidity}%`;
+    document.querySelector('#wind').innerHTML = `${json.windSpeed.toFixed(1)}Km/h`;
 }
 
 function showAlert(msg) {
